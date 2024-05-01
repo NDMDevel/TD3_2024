93f565781ae81adc74dd65567166d223aab30e4f
NDMDevel <damian.n.marasco@gmail.com>
2024-04-30 21:09:59 -0300

    gauge esp32 server test

---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
+
+//SPIFFS (obsoleto, no permite subdirectorios)
+//SPI Flash File System
+
+//LittleFS (subdirectorios)
+
+uint16_t tcp_port = 80;
+AsyncWebServer server( tcp_port );
+
+const char sta_ssid[] = "LAB_ELECTRONICA";
+const char sta_pass[] = "LabElec2022";
+const char ap_ssid[]  = "TD3_AP_Test";
+const char ap_pass[]  = "password";
+
+const IPAddress ap_ip(10,10,10,10);
+const IPAddress ap_gateway(10,10,10,1);
+const IPAddress ap_subnet(255,255,255,0);
+
+void home_handler(AsyncWebServerRequest *request)
+{
+    const char html[] = R"=====(
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ESP32 Title</title>
+</head>
+<body>
+    <H1>Hola</H1><br>
+    <input type="button"
+           value="Click me"
+           onclick="js_function()">    
+    <!-- very easy -->
+</body>
+<script>
+//javascript (C/C++)
+function js_function()
+{
+    console.log("hello console!!");
+}
+</script>
+<style>
+
+</style>
+</html>
+    )=====";
+    request->send(200,"text/html",html);
+}
+
+void home1_handler(AsyncWebServerRequest *request)
+{
+    request->send(SPIFFS,"/home.html");
+}
+
+void gauge_handler(AsyncWebServerRequest *request)
+{
+    request->send(SPIFFS,"/gauge_test.html");
+}
+void gauge_lib_handler(AsyncWebServerRequest *request)
+{
+    request->send(SPIFFS,"/gauge.min.js");
+}
+void setup()
+{
+    Serial.begin(115200);//8N1
+
+    WiFi.softAPConfig( ap_ip , ap_gateway , ap_subnet );
+    WiFi.softAP( ap_ssid , ap_pass );
+
+    if( !SPIFFS.begin() )
+        Serial.println("Error SPIFFS not working");
+
+    server.on("/home" ,home_handler);
+    server.on("/home1",home1_handler);
+    server.on("/gauge",gauge_handler);
+    server.on("/gauge.min.js",gauge_lib_handler);
+
+    //server listo para recibir peticiones (de paginas web)
+    server.begin();
+}
+
+void loop()
+{
+
+    delay(1000);
+    Serial.println("Hello world!\n");
+}
